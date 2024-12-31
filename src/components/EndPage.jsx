3e4df4a98250bc7b5b44b2e1932fdbbb2c7f3e2b
Your Name <you@example.com>
2024-12-31 16:18:02 +0100

    SocialMedia

--- src/components/EndPage.jsx
+++ src/components/EndPage.jsx
@@ -8,10 +8,18 @@ const EndPage = () => {
     <div className="w-full flex justify-center items-center bottom-0">
       <div className="bg-[#09041d] h-32 w-full flex justify-center  py-4 shadow-2xl shadow-white">
         <div className="grid grid-cols-4 gap-4 justify-items-center">
-          <AvatarSocialMediaWhite icon={<FaGithub size={20} />} />
-          <AvatarSocialMediaWhite icon={<FaInstagram size={20} />} />
-          <AvatarSocialMediaWhite icon={<FaLinkedin size={20} />} />
-          <AvatarSocialMediaWhite icon={<FaFacebook size={20} />} />
+          <a href="https://github.com/AkramAn2002">
+            <AvatarSocialMediaWhite icon={<FaGithub size={20} />} />
+          </a>
+          <a href="https://www.instagram.com/akramanou/">
+            <AvatarSocialMediaWhite icon={<FaInstagram size={20} />} />
+          </a>
+          <a href="https://www.linkedin.com/in/akram-anou-9493aa252/">
+            <AvatarSocialMediaWhite icon={<FaLinkedin size={20} />} />
+          </a>
+          <a href="https://www.facebook.com/akram.anou.33">
+            <AvatarSocialMediaWhite icon={<FaFacebook size={20} />} />
+          </a>
         </div>
         <div className="absolute flex justify-center space-x-3 text-white mt-[55px] text-[10px] sm:text-[13px] md:text-[15px]">
           <span className="cursor-pointer">FAQ</span>
--- src/components/SocialMedias.jsx
+++ src/components/SocialMedias.jsx
@@ -5,10 +5,18 @@ import AvatarSkil from "./AvatarSocialMedia";
 const SocialMedias = () => {
   return (
     <div className="grid grid-cols-4 lg:gap-2">
-      <AvatarSkil icon={<FaGithub size={30} />} />
-      <AvatarSkil icon={<FaInstagram size={30} />} />
-      <AvatarSkil icon={<FaLinkedin size={30} />} />
-      <AvatarSkil icon={<FaFacebook size={30} />} />
+      <a href="https://github.com/AkramAn2002">
+        <AvatarSkil icon={<FaGithub size={30} />} />
+      </a>
+      <a href="https://www.instagram.com/akramanou/">
+        <AvatarSkil icon={<FaInstagram size={30} />} />
+      </a>
+      <a href="https://www.linkedin.com/in/akram-anou-9493aa252/">
+        <AvatarSkil icon={<FaLinkedin size={30} />} />
+      </a>
+      <a href="https://www.facebook.com/akram.anou.33">
+        <AvatarSkil icon={<FaFacebook size={30} />} />
+      </a>
     </div>
   );
 };
